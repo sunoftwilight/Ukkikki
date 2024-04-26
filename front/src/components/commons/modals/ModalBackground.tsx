96b097a3047c18c519c442a855da66f1313dacb6
이해진 <sixb_0417@hanmail.net>
2024-04-26 10:12:51 +0900

    [feat] 모달백그라운드 CSS 구현 완료
    Change-Id: I6438f5d3fddb32f8a999e32c8e7fd0eb04f70b95

---
+++ front/src/components/commons/modals/ModalBackground.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ModalBackground: React.FC = () => {
+  return (
+    <div className="z-10 fixed w-full h-full flex justify-center items-center bg-white/5 shadow-inner backdrop-blur-sm">
+    </div>
+  )
+};
+
+export default ModalBackground;
