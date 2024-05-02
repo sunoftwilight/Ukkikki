33765ce1b1c9d880c87ebd8f1d11e2a15d907378
박병조 <k97115284@gmail.com>
2024-05-02 21:36:45 +0800

    [FEAT] 그룹생성화면 완료
    Change-Id: I95bbce1813ffb125e4b54ddcbe3256b1ae0e04b7
    #S10P31D202-215 #Done
    #S10P31D202-216 #Done

--- front/src/components/GroupCreate/InsertInfo.tsx
+++ front/src/components/GroupCreate/InsertInfo.tsx
@@ -7,13 +7,16 @@ interface InsertInfoProps {
 
 
 const InsertInfo:React.FC<InsertInfoProps> = ({ onNextBtnClick }) => {
-  const [selectedImage, setSelectedImage] = useState<String>('');
+  const [selectedImage, setSelectedImage] = useState<string>('');
   const inputImgRef = useRef<HTMLInputElement>(null);
 
-
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const files = e.target.files?.[0];
-    
+    const file = e.target.files?.[0];
+
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedImage(imageUrl);
+    }
   };
 
 
@@ -22,11 +25,28 @@ const InsertInfo:React.FC<InsertInfoProps> = ({ onNextBtnClick }) => {
   }
 
   return (
-    <div className="flex flex-col w-full h-full">
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      <button className="w-full h-[60px] bg-main-blue rounded-2xl" onClick={handleNextBtnClick}>
-        <p>다음</p>
-      </button>
+    <div className="flex flex-col w-full h-full items-center">
+      <div className="w-full mb-14">
+        <p className="text-3xl ps-4">그룹 대표정보</p>
+      </div>
+      <div className="mb-10">
+        <img
+          className="w-36 h-36 rounded-full bg-gray"
+          src={selectedImage}
+          onClick={() => inputImgRef.current?.click()}/>
+        <input
+          className="hidden"
+          type="file"
+          accept="image/*"
+          ref={inputImgRef}
+          onChange={handleImageChange}/>
+      </div>
+      <div className="px-4 w-full">
+        <input type="text" className="w-full h-[60px] bg-soft-gray rounded-2xl mb-3 text-xl font-pre-SB text-center focus:none" />
+        <button className="w-full h-[60px] bg-main-blue rounded-2xl" onClick={handleNextBtnClick}>
+          <p className="font-pre-B text-xl text-white">다음</p>
+        </button>
+      </div>
     </div>
   )
 }
