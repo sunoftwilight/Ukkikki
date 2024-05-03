de6d6406cb3aab6f6ced97094c1b0f6b6219f44c
박병조 <k97115284@gmail.com>
2024-05-03 17:53:20 +0900

    [FEAT] 참여페이지 작업중
    Change-Id: I08b62207dc324144fbec7fb4550cf270f1964836

---
+++ front/src/pages/GroupAttend.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const GroupAttend:React.FC = () => {
+  return (
+    <div className="flex flex-col w-full h-full px-4">
+        그룹 참여
+    </div>
+  )
+}
+
+export default GroupAttend;
+
