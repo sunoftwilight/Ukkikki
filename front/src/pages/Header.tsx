328bc76ba0b653193540bcba1e08df0b36d11a2e
이해진 <sixb_0417@hanmail.net>
2024-05-03 17:10:10 +0900

    [feat] 게시글 상세 페이지에서 사진을 클릭할 경우 보이는 이미지 상세 페이지 CSS
    Change-Id: I9a62d2d856378d83337bc9226321927b372a704d

--- front/src/pages/Header.tsx
+++ front/src/pages/Header.tsx
@@ -4,6 +4,7 @@ import AlbumHeader from "../components/Header/AlbumHeader";
 import BackHeader from "../components/Header/BackHeader";
 import LogoHeader from "../components/Header/LogoHeader";
 import WriteHeader from "../components/Header/WriteHeader";
+import SaveHeader from "../components/Header/SaveHeader";
 
 const Header: React.FC = () => {
   const location = useLocation()
@@ -13,6 +14,7 @@ const Header: React.FC = () => {
   const albumPath = ['/album']
 
   if (basicPath.includes(location.pathname)) return <LogoHeader />
+  else if (location.pathname.startsWith('/feed/img/')) return <SaveHeader />
   else if (backPath.includes(location.pathname) || location.pathname.startsWith('/album/') || location.pathname.startsWith('/feed/')) return <BackHeader />
   else if (albumPath.includes(location.pathname)) return <AlbumHeader />
   else if (location.pathname === '/write') return <WriteHeader />
