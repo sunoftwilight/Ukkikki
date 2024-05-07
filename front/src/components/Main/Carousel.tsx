235a688b0465deaf50d3eb05626687432f8b750a
이해진 <sixb_0417@hanmail.net>
2024-05-07 10:51:44 +0900

    [fix] 캐러셀 적용 (swiper 라이브러리 사용)
    Change-Id: I4edd4292f1e631e053ff24430db1c5a0756b0448

--- front/src/components/Main/Carousel.tsx
+++ front/src/components/Main/Carousel.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import 'swiper/css'
+import 'swiper/css/pagination'
 import carousel1 from '@/assets/Main/Carousel/carousel1.png'
+import carousel2 from '@/assets/Main/Carousel/carousel2.png'
+import carousel3 from '@/assets/Main/Carousel/carousel3.png'
 
 const Carousel: React.FC = () => {
   return (
-    <div className="w-full">
-      <img src={carousel1} alt="carousel1" />
-    </div>
+    <Swiper 
+      className="w-full"
+      spaceBetween={50}
+      pagination={{clickable: true}}
+      modules={[Pagination, Autoplay]}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+    >
+      <SwiperSlide><img src={carousel1} alt="carousel1" className="w-full" /></SwiperSlide>
+      <SwiperSlide><img src={carousel2} alt="carousel2" className="w-full" /></SwiperSlide>
+      <SwiperSlide><img src={carousel3} alt="carousel3" className="w-full" /></SwiperSlide>
+    </Swiper>
   )
 };
 
