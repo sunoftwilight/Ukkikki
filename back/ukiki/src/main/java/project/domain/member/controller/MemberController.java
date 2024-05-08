727f673deb66165b3ced475f84f9011d26087b6d
양성규 <yt1356@naver.com>
2024-05-08 13:18:31 +0900

    [FEAT] : myinfo swagger 등록
    Change-Id: I516df9c8f975c3ce49d09bd64de5c41950b41d3a

--- back/ukiki/src/main/java/project/domain/member/controller/MemberController.java
+++ back/ukiki/src/main/java/project/domain/member/controller/MemberController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.domain.member.dto.response.InfoDto;
 import project.domain.member.service.MemberService;
+import project.global.result.ResultCode;
+import project.global.result.ResultResponse;
 
 import java.util.Optional;
 
@@ -13,17 +15,17 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
-public class MemberController {
+public class MemberController implements MemberDocs{
 
     private final MemberService memberService;
 
     // 내 정보 조회
     @GetMapping("/info/my")
-    public ResponseEntity<?> myInfo() {
+    public ResponseEntity<ResultResponse> myInfo() {
         // 로그인 성공했을때 저장해두었던 값을들 가져온다.
         try {
             InfoDto infoDTO = memberService.myInfo();
-            return ResponseEntity.status(HttpStatus.OK).body(infoDTO);
+            return ResponseEntity.ok(new ResultResponse(ResultCode.GET_USERINFO_SUCCESS, infoDTO));
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
