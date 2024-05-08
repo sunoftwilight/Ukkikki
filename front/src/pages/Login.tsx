4ed08f29af8a8628546a9ca1b681f951be8e1f71
박병조 <k97115284@gmail.com>
2024-05-09 00:59:54 +0800

    [TEST] 로그인 로직 테스트
    Change-Id: Id251e29b5b94bb0e7e887346ba42fcad5fe3c2d5

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -2,60 +2,28 @@ import React, { useEffect } from 'react';
 import LoginLogo from "../../icons/256.png";
 import LoginBtn from "@/assets/Login/kakaoLoginBtn.png";
 
-import { useCookies } from 'react-cookie';
-import { useNavigate } from 'react-router';
-import { userStore } from '../stores/UserStore';
-import { useStore } from 'zustand';
+// import { useCookies } from 'react-cookie';
+// import { useNavigate } from 'react-router';
+// import { userStore } from '../stores/UserStore';
+// import { useStore } from 'zustand';
 
-import { tokenRefresh, userInfo } from '../api/user';
+// import { tokenRefresh, userInfo } from '../api/user';
 
 
 const Login: React.FC = () => {
-  const [cookies] = useCookies(['isLogin']);
-  const navi = useNavigate();
-  const user = useStore(userStore);
-
-  useEffect(() => {
-    getAccess();
-    getUserInfo();
-  }, [])
-
-  const getAccess = async () => {
-    if (cookies.isLogin === 'true'){
-      await tokenRefresh(
-        (response) => {
-          user.setAccessToken(response.headers['access']);
-          user.setIsLogin(true);
-        },
-        (error) => {
-          console.error(error)
-        }
-      )
-    }
+  // const [cookies] = useCookies(['isLogin']);
+  // const navi = useNavigate();
+  // const accessToken = userStore.getState().accessToken;
+  // const isLogin = userStore.getState().isLogin;
+  const login = () => {
+    window.location.href = "https://k10d202.p.ssafy.io/api/oauth2/authorization/kakao"
   }
 
-  const getUserInfo = async () => {
-    if (user.accessToken !== '') {
-      await userInfo(
-        (response) => {
-          const userData = response.data.data;
-          user.setUserId(userData.userId);
-          user.setUserName(userData.userName);
-          user.setUserProfile(userData.profileUrl);
-        },
-        (error) => {
-          console.error(error)
-        }
-      )
-    }
-    navi('/')
-  }
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-white">
       <img src={LoginLogo} className="-mt-11 mb-5"/>
-      <a href="https://k10d202.p.ssafy.io/api/oauth2/authorization/kakao">
-        <img src={LoginBtn} />
-      </a>
+      <img src={LoginBtn} onClick={() => login()}/>
     </div>
   )
 };
