4954fe3eee79e8b8ab89dd391717a6b42d14b695
박병조 <k97115284@gmail.com>
2024-05-09 09:31:20 +0900

    [TEST] 로그인 테스트 로직추가
    Change-Id: I0e1991eef663ca5a58c19c78e536076cdd4a7223

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LoginLogo from "../../icons/256.png";
 import LoginBtn from "@/assets/Login/kakaoLoginBtn.png";
 
-// import { useCookies } from 'react-cookie';
-// import { useNavigate } from 'react-router';
-// import { userStore } from '../stores/UserStore';
-// import { useStore } from 'zustand';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router';
+import { userStore } from '../stores/UserStore';
 
-// import { tokenRefresh, userInfo } from '../api/user';
+import { useStore } from 'zustand';
+import { tokenRefresh, userInfo } from '../api/user';
 
 
 const Login: React.FC = () => {
-  // const [cookies] = useCookies(['isLogin']);
-  // const navi = useNavigate();
-  // const accessToken = userStore.getState().accessToken;
-  // const isLogin = userStore.getState().isLogin;
+  const [cookies] = useCookies(['isLogin']);
+  const navi = useNavigate();
+  const isLogin = userStore.getState().isLogin;
+  const user = useStore(userStore);
   const login = () => {
     window.location.href = "https://k10d202.p.ssafy.io/api/oauth2/authorization/kakao"
   }
+
+  useEffect(() => {
+    if(cookies.isLogin === 'true' && !userStore.getState().isLogin) autoCheck();
+  }, [])
+
+  const autoCheck = async () => {
+    await tokenRefresh(
+      async (response) => {
+        if (!isLogin) {
+          user.setAccessToken(response.headers.access)
+          await userInfo(
+            (response) => {
+              const userData = response.data.data;
+              user.setUserId(userData.userId);
+              user.setUserName(userData.userName);
+              user.setUserProfile(userData.profileUrl);
+            },
+            (err) => {console.log(err)}
+          );
+          user.setIsLogin(true);
+          navi('/');
+        }
+      },
+      () => {}
+    );
+  };
 
 
   return (
