b2a4793ffcc7b455cfd7b79f74add9c5a57b2010
이해진 <sixb_0417@hanmail.net>
2024-05-09 10:45:20 +0900

    [chore] 빌드 에러 수정
    Change-Id: Idaa3b223e6a9baf04824c5a13aa8642718a1cf4c

--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+import React from "react";
 import Banner from "../components/Main/Banner";
 import Carousel from "../components/Main/Carousel";
 import Buttons from "../components/Main/Buttons";
