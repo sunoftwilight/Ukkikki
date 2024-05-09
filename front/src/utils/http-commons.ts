e29831e8cca8529b0c0ac1f1781a297021a8ae53
박병조 <k97115284@gmail.com>
2024-05-09 10:12:33 +0900

    [TEST] 로그인 테스트 로직추가
    Change-Id: I7e7345f1d80506a0c1d1d0ecb316c8ae68735b67

--- front/src/utils/http-commons.ts
+++ front/src/utils/http-commons.ts
@@ -1,14 +1,14 @@
 import axios, { AxiosInstance } from "axios";
 import { userStore } from "../stores/UserStore";
-import { useStore } from "zustand";
-import { tokenRefresh } from "../api/user";
-import { httpStatusCode } from "./http-status";
+
+// import { tokenRefresh } from "../api/user";
+// import { httpStatusCode } from "./http-status";
 
 axios.defaults.withCredentials = true;
 
 const baseURL: string = "https://k10d202.p.ssafy.io/api";
 const accessToken = userStore.getState().accessToken;
-const user = useStore(userStore)
+
 export const publicApi: AxiosInstance = axios.create({
 	baseURL: baseURL,
 	headers: {
@@ -46,42 +46,43 @@ export const formDataApi: AxiosInstance = axios.create({
 });
 
 
-
 privateApi.interceptors.request.use(
   (config) => {
-    const token = user.accessToken
-    if (token !== '') {
-      // localStorage에 access 토큰이 있으면 요청 헤더에 추가
-      config.headers['access'] = token;
-    }
+		const stored = localStorage.getItem('USER_STORE');
+		if (stored){
+			const obj = JSON.parse(stored)
+			if (obj.state.accessToken !== ''){
+				config.headers['access'] = obj.state.accessToken;
+			}
+		}
     return config;
   },
-  async (error) => {
-    const { config, response: { status }, } = error;
-    // 토큰 만료일 경우.
-    if (status === 401) {
-      if (error.response.data.message === 'access token expired') {
-        const originRequest = config;
+  async () => {
+    // const { config, response: { status }, } = error;
+    // // 토큰 만료일 경우.
+    // if (status === 401) {
+    //   if (error.response.data.message === 'access token expired') {
+    //     const originRequest = config;
 
-        // 토큰 재발급.
-        await tokenRefresh(
-          (res) => {
-            // 성공 시
-            if (res.status === httpStatusCode.OK && res.headers.access) {
-              localStorage.setItem('accessToken', res.headers.access);
-              axios.defaults.headers.access = `${res.headers.access}`;
-              originRequest.headers.access = `${res.headers.access}`;
+    //     // 토큰 재발급.
+    //     await tokenRefresh(
+    //       (res) => {
+    //         // 성공 시
+    //         if (res.status === httpStatusCode.OK && res.headers.access) {
+    //           localStorage.setItem('accessToken', res.headers.access);
+    //           axios.defaults.headers.access = `${res.headers.access}`;
+    //           originRequest.headers.access = `${res.headers.access}`;
 
-              // 토큰 교환 후 재 시도.
-              return axios(originRequest);
-            }
-          },
-          () => {
+    //           // 토큰 교환 후 재 시도.
+    //           return axios(originRequest);
+    //         }
+    //       },
+    //       () => {
             
-          }
-        )
-      }
-    }
+    //       }
+    //     )
+    //   }
+    // }
   }
 );
 
