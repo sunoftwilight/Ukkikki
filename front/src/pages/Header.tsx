87e56e1f4dd49a2093e0146420d73116748997a3
이해진 <sixb_0417@hanmail.net>
2024-05-10 16:49:33 +0900

    [test] SSE 연결 테스팅 중
    Change-Id: I495eaee36dd714cbdc8e36acf5f4073ccdf404aa

--- front/src/pages/Header.tsx
+++ front/src/pages/Header.tsx
@@ -6,6 +6,7 @@ import LogoHeader from "../components/Header/LogoHeader";
 import WriteHeader from "../components/Header/WriteHeader";
 import SaveHeader from "../components/Header/SaveHeader";
 import { connectAlarm } from "../api/alarm";
+import { EventSourcePolyfill } from 'event-source-polyfill';
 
 const Header: React.FC = () => {
 	const location = useLocation();	
@@ -28,6 +29,26 @@ const Header: React.FC = () => {
 				console.error("연결 실패", err);
 			},
 		);
+		const eventSource = new EventSourcePolyfill('/api/alarm/sub',{
+			headers: {
+				'authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImlkIjoxLCJ1c2VybmFtZSI6IuyEseq3nCIsInByb3ZpZGVySWQiOiJrYWthbyAzNDU4Njg5NDM3IiwiaWF0IjoxNzE1MjM1ODk5LCJleHAiOjE3MTYwOTk4OTl9.mdm4F9ymRYeyAKJcds4sl1_j_g-5oRfSMkQZJBcNVHk`,
+			},
+		});
+	
+		eventSource.addEventListener('connect', () => {
+			//'new_thread' 이벤트가 오면 할 동작
+			console.log('연결')
+		});
+	
+		eventSource.onerror = () => {
+			//에러 발생시 할 동작
+			console.log('에러')
+			eventSource.close(); //연결 끊기
+		};
+		
+		return () => {
+			eventSource.close();
+		};
 	}, []);
 
 	if (basicPath.includes(location.pathname)) return <LogoHeader />;
@@ -43,3 +64,19 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+
+
+
+// "id:1_1715326312683
+// event:CHECK
+// data:{
+// 	"partyId":null,
+// 	"contentsId":null,
+// 	"targetId":null,
+// 	"alarmType":"CHECK",
+// 	"content":null,
+// 	"createDate":null,
+// 	"identifier":null,
+// 	"read":false
+// }
+// "
