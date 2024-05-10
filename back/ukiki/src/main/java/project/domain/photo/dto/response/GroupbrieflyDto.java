3c82f13737b51bd690c399d3ddf7d7715fc4fb6e
박지훈 <wlgns3479@hanmail.net>
2024-05-10 18:01:19 +0900

    [SAVE]
    Change-Id: I0c44b23ce44330f7b0ad838967ca2f1395a8fc80

---
+++ back/ukiki/src/main/java/project/domain/photo/dto/response/GroupbrieflyDto.java
@@ -0,0 +1,14 @@
+package project.domain.photo.dto.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GroupbrieflyDto {
+
+    private String groupName;
+    private String thumbnailUrl;
+
+}
--- back/ukiki/src/main/java/project/domain/photo/repository/PhotoRepository.java
+++ back/ukiki/src/main/java/project/domain/photo/repository/PhotoRepository.java
@@ -1,11 +1,22 @@
 package project.domain.photo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import project.domain.photo.entity.MetaCode;
 import project.domain.photo.entity.Photo;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
     Optional<Photo> findById(long id);
+
+    @Query("select MAX(p.id), p.photoUrl.thumb_url1 from Photo p join p.metaList m where m.metaCode = :metaCode")
+    List<Photo> findByMetaCode(@Param("metaCode") MetaCode metaCode);
+
+//    @Query("select MAX(p.id), p.photoUrl.thumb_url1 from Photo p join p.metaList m where m.metaCode = :metaCode")
+//    List<Photo> findByMetaCode(@Param("metaCode") MetaCode metaCode);
 
 }
