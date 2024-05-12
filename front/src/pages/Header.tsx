755dddbbe9e73c93841bfdfe2d18a9b69c348503
sun <sixb_0417@hanmail.net>
2024-05-13 00:23:54 +0900

    [fix] 그룹 라우팅 변경으로 인한 헤더 사라짐 현상 수정
    Change-Id: I55b9397fcc949221a2bc8f348e59f984e6fecdac

--- front/src/pages/Header.tsx
+++ front/src/pages/Header.tsx
@@ -29,13 +29,17 @@ const Header: React.FC = () => {
 	const albumPath = ["/album", "/trash"];
 
 	useEffect(() => {
-		const sse = new EventSourcePolyfill("https://k10d202.p.ssafy.io/api/alarm/sub", {
-			headers: {
-				authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImlkIjoxLCJ1c2VybmFtZSI6IuyEseq3nCIsInByb3ZpZGVySWQiOiJrYWthbyAzNDU4Njg5NDM3IiwiaWF0IjoxNzE1MjM1ODk5LCJleHAiOjE3MTYwOTk4OTl9.mdm4F9ymRYeyAKJcds4sl1_j_g-5oRfSMkQZJBcNVHk",
-				"Content-Type": "text/event-stream",
+		const sse = new EventSourcePolyfill(
+			"https://k10d202.p.ssafy.io/api/alarm/sub",
+			{
+				headers: {
+					authorization:
+						"Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsImlkIjoxLCJ1c2VybmFtZSI6IuyEseq3nCIsInByb3ZpZGVySWQiOiJrYWthbyAzNDU4Njg5NDM3IiwiaWF0IjoxNzE1MjM1ODk5LCJleHAiOjE3MTYwOTk4OTl9.mdm4F9ymRYeyAKJcds4sl1_j_g-5oRfSMkQZJBcNVHk",
+					"Content-Type": "text/event-stream",
+				},
 			},
-		});
-		
+		);
+
 		sse.addEventListener("CHECK", (event: any) => {
 			const e = event as MessageEvent; // 이벤트 타입을 MessageEvent로 캐스팅
 			console.log("connect event data: ", e.data); // 이제 e.data를 안전하게 사용할 수 있습니다.
@@ -49,30 +53,21 @@ const Header: React.FC = () => {
 		return () => sse.close(); // 컴포넌트 언마운트 시 EventSource 연결을 닫습니다.
 	}, []);
 
-	if (basicPath.includes(location.pathname)) return <LogoHeader />;
-	else if (location.pathname.startsWith("/feed/img/")) return <SaveHeader />;
-	else if (
+	if (
 		backPath.includes(location.pathname) ||
 		location.pathname.startsWith("/album/") ||
-		location.pathname.startsWith("/feed/")
+		location.pathname.startsWith("/feed/") ||
+		location.pathname.startsWith("/group/list")
 	)
 		return <BackHeader />;
+	else if (
+		basicPath.includes(location.pathname) ||
+		location.pathname.startsWith("/group/")
+	)
+		return <LogoHeader />;
+	else if (location.pathname.startsWith("/feed/img/")) return <SaveHeader />;
 	else if (albumPath.includes(location.pathname)) return <AlbumHeader />;
 	else if (location.pathname === "/write") return <WriteHeader />;
 };
 
 export default Header;
-
-// "id:1_1715326312683
-// event:CHECK
-// data:{
-// 	"partyId":null,
-// 	"contentsId":null,
-// 	"targetId":null,
-// 	"alarmType":"CHECK",
-// 	"content":null,
-// 	"createDate":null,
-// 	"identifier":null,
-// 	"read":false
-// }
-// "
--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -28,7 +28,7 @@ const Main: React.FC = () => {
         <Carousel />
       </div>
 
-      <div className="flex flex-col gap-4 h-full">
+      <div className="flex flex-col gap-4 h-full pb-4">
         {/* 유저 정보 */}
         <div>
           <div className="font-pre-SB text-lg text-black">
