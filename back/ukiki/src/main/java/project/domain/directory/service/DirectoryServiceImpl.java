9c79f3b2c902f92216fc66beab9676fccf7c5966
newtkdtn <newtkdtn@gmail.com>
2024-05-13 17:08:59 +0900

    [FEAT] : GetDirThumbUrl2 Dto 변경
    Change-Id: I5222e0951e17322b66e8d2c9bd9a35931798942e

--- back/ukiki/src/main/java/project/domain/directory/service/DirectoryServiceImpl.java
+++ back/ukiki/src/main/java/project/domain/directory/service/DirectoryServiceImpl.java
@@ -612,6 +612,7 @@ public void deleteDir(String dirId) { // photo의 경우도 고려해줘야 한�
                 GetDirThumbUrl2.builder()
                     .pk(file.getId())
                     .thumbUrl2(file.getPhotoDto().getThumbUrl2())
+                    .photoUrl(file.getPhotoDto().getPhotoUrl())
                     .build());
         }
         log.info("service response = {}", response);
