8e0ff9ef4a168ccdc971aa20a46f2c19ca6b67ea
최용수 <cys980220@gmail.com>
2024-05-17 14:17:55 +0900

    [!HOTFIX] : Security WebSocket Header ADD
    Change-Id: Ibe26dc05f3e5a3b4c6747ed989c76f36a8864e71

--- back/ukiki/src/main/java/project/global/config/SecurityConfig.java
+++ back/ukiki/src/main/java/project/global/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import project.global.jwt.JWTFilter;
 import project.global.jwt.JWTUtil;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -74,10 +75,10 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "https://k10d202.p.ssafy.io")); // 모든 출처 허용
-        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(
             Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드 지정
-        configuration.setAllowedHeaders(Arrays.asList("User-Agent","Sec-Ch-Ua-Platform","Sec-Ch-Ua-Mobile","Sec-Ch-Ua","Referer","X-Frame-Options","Sec-WebSocket-Extensions","Sec-WebSocket-Version","Connection","Upgrade","Sec-Websocket-Key","Authorization","text/event-stream" ,"authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "password", "sseKey")); // 모든 헤더 허용
+        configuration.setAllowedHeaders(Arrays.asList("Sec-Websocket-Version","Sec-Websocket-Extensions","Pragma","Host","Cache-Control","Accept-Language","Accept-Encoding","User-Agent","Sec-Ch-Ua-Platform","Sec-Ch-Ua-Mobile","Sec-Ch-Ua","Referer","X-Frame-Options","Sec-WebSocket-Extensions","Sec-WebSocket-Version","Connection","Upgrade","Sec-Websocket-Key","Authorization","text/event-stream" ,"authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers", "password", "sseKey")); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 크레덴셜 허용
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
