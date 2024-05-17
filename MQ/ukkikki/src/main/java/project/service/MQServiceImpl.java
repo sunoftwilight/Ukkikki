f7be2b9edc66f5d27dd48c9cb2cc44ae8dc1ba0e
양성규 <yt1356@naver.com>
2024-05-17 15:16:15 +0900

    log
    Change-Id: I80f2931fc9cb261336dd444fd3bb1472532650f8

--- MQ/ukkikki/src/main/java/project/service/MQServiceImpl.java
+++ MQ/ukkikki/src/main/java/project/service/MQServiceImpl.java
@@ -40,8 +40,8 @@ public class MQServiceImpl implements MQService {
 
         Long partyId = mqDto.getPartyId();
 
-        log.info("짝수 큐 : " + evenLinkedDeque.size());
-        log.info("홀수 큐 : " + oddLinkedDeque.size());
+        log.info("업로드 짝수 큐 : " + evenLinkedDeque.size());
+        log.info("업로드 홀수 큐 : " + oddLinkedDeque.size());
         /*
         홀수 짝수 구분 후 큐에 넣어준다.
         만약 큐가 비어있었다면
