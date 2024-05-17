3b7a38fc0247134853d8c050e489fcd217c03207
양성규 <yt1356@naver.com>
2024-05-17 16:11:58 +0900

    log
    Change-Id: I7735c042878afaf17ee11873aef1001f2b9eb998

--- MQ/ukkikki/src/main/java/project/service/MQServiceImpl.java
+++ MQ/ukkikki/src/main/java/project/service/MQServiceImpl.java
@@ -19,70 +19,75 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 @Service
 @Slf4j
 public class MQServiceImpl implements MQService {
 
-    private final BlockingQueue<MQDto> evenQueue;
-    private final BlockingQueue<MQDto> oddQueue;
+    private final ConcurrentLinkedDeque<MQDto> evenLinkedDeque;
+    private final ConcurrentLinkedDeque<MQDto> oddLinkedDeque;
 
     private final WebClient webClient;
-
     @Autowired
-    public MQServiceImpl(BlockingQueue<MQDto> evenQueue, BlockingQueue<MQDto> oddQueue, WebClient webClient) {
-        this.evenQueue = evenQueue;
-        this.oddQueue = oddQueue;
+    public MQServiceImpl(ConcurrentLinkedDeque<MQDto> evenLinkedDeque, ConcurrentLinkedDeque<MQDto> oddLinkedDeque,WebClient webClient) {
+        this.evenLinkedDeque = evenLinkedDeque;
+        this.oddLinkedDeque = oddLinkedDeque;
         this.webClient = webClient;
     }
 
     @Override
     public void fileUpload(MQDto mqDto) {
-        Long partyId = mqDto.getPartyId();
-        log.info("업로드 짝수 큐 : " + evenQueue.size());
-        log.info("업로드 홀수 큐 : " + oddQueue.size());
 
-        switch ((int) (partyId % 2)) {
+        Long partyId = mqDto.getPartyId();
+
+        log.info("업로드 짝수 큐 : " + evenLinkedDeque.size());
+        log.info("업로드 홀수 큐 : " + oddLinkedDeque.size());
+        /*
+        홀수 짝수 구분 후 큐에 넣어준다.
+        만약 큐가 비어있었다면
+        바로 실행할 수 있게 해준다.
+         */
+        switch ((int) (partyId%2)){
             case 0:
-                evenQueue.offer(mqDto);
-                if (evenQueue.size() == 1) {
+                evenLinkedDeque.offerLast(mqDto);
+                if(evenLinkedDeque.size() == 1){
                     fileAiUpload(0);
                 }
                 break;
             case 1:
-                oddQueue.offer(mqDto);
-                if (oddQueue.size() == 1) {
+                oddLinkedDeque.offerLast(mqDto);
+                if(oddLinkedDeque.size() == 1){
                     fileAiUpload(1);
                 }
                 break;
         }
+
+
     }
 
     @Override
     public void fileAiUpload(int index) {
+
         log.info("index : " + index + "의 작업이 시작됩니다.");
 
-        log.info("짝수 큐 : " + evenQueue.size());
-        log.info("홀수 큐 : " + oddQueue.size());
+        log.info("짝수 큐 : " + evenLinkedDeque.size());
+        log.info("홀수 큐 : " + oddLinkedDeque.size());
 
-        MQDto mqDto;
-        try {
-            mqDto = index == 0 ? evenQueue.take() : oddQueue.take();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        MQDto mqDto = index == 0 ? evenLinkedDeque.peekFirst() : oddLinkedDeque.peekFirst();
+
+        if(mqDto == null)
             return;
-        }
 
         MultipartFile multipartFile = mqDto.getFile(); // mqDto에서 MultipartFile을 가져옴
+
 
         log.info("파일 정보 : " + multipartFile.getOriginalFilename());
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
             multipartFile.getInputStream().transferTo(baos); // MultipartFile의 내용을 바이트 배열로 변환
         } catch (IOException e) {
-            log.error("Error occurred during file conversion. File path: " + multipartFile.getOriginalFilename(), e);
+                log.error("Error occurred during file conversion. File path: " + multipartFile.getOriginalFilename(), e);
         }
 
         // 바이트 배열 저장
@@ -130,15 +135,34 @@ public class MQServiceImpl implements MQService {
     @Override
     public void finish(int index) {
         log.info("index의 작업을 제거합니다");
-        // 인덱스에 따라 다음 작업을 실행합니다.
+        // 인덱스에 따라 값을 제거해준다.
         if (index == 0) {
-            if (!evenQueue.isEmpty()) {
+            evenLinkedDeque.pollFirst();
+
+            // 큐에 값이 남아있다면 반복한다.
+            if(!evenLinkedDeque.isEmpty()){
                 fileAiUpload(index);
             }
+
         } else {
-            if (!oddQueue.isEmpty()) {
+            oddLinkedDeque.pollFirst();
+
+            if(!oddLinkedDeque.isEmpty()){
                 fileAiUpload(index);
             }
+
         }
+
     }
+
+
+//    @Override
+//    public void queSize() {
+//        System.out.println(waitLinkedDeque.size());
+//        for(int i=0;i<workLinkedDeque.length;i++){
+//            System.out.println("workLinkedDeque" + i + " = " + workLinkedDeque[i].size());
+//        }
+//    }
+
+
 }
