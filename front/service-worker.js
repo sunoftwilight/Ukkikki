425b4d6cd2c2d598384c1bb3be774a44f961678e
이해진 <sixb_0417@hanmail.net>
2024-05-17 15:36:17 +0900

    [fix] service-workers hotfix

--- front/service-worker.js
+++ front/service-worker.js
@@ -1,27 +1,39 @@
-const CACHE_NAME = 'cache-v1';
+// const CACHE_NAME = 'cache-v1';
 
-const FILES_TO_CACHE = ['/offline.html'];
+// // const FILES_TO_CACHE = ['/offline.html'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+// self.addEventListener('install', event => {
+//   // event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+// });
+
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys().then(keyList =>
+//       Promise.all(
+//         keyList.map(key => {
+//           if (CACHE_NAME !== key) return caches.delete(key);
+//         }),
+//       ),
+//     ),
+//   );
+// });
+
+// self.addEventListener('fetch', event => {
+//   if ('navigate' !== event.request.mode) return;
+
+//   event.respondWith(
+//     fetch(event.request).catch(() => caches.open(CACHE_NAME).then(cache => cache.match('/offline.html'))),
+//   );
+// });
+
+// install event
+self.addEventListener("install", () => {
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (CACHE_NAME !== key) return caches.delete(key);
-        }),
-      ),
-    ),
-  );
+// activate event
+self.addEventListener("activate", () => {
 });
 
-self.addEventListener('fetch', event => {
-  if ('navigate' !== event.request.mode) return;
-
-  event.respondWith(
-    fetch(event.request).catch(() => caches.open(CACHE_NAME).then(cache => cache.match('/offline.html'))),
-  );
+// fetch event
+self.addEventListener("fetch", () => {
 });
