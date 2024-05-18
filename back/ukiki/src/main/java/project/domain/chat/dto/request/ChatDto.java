b2f0487cf4733a75baefd10b6a244617a66d029a
최용수 <cys980220@gmail.com>
2024-05-18 22:31:08 +0900

    [!HOTFIX] : 채팅 암호화 투스트링
    Change-Id: I786359ff6594916186c2a7405358174858a18083

--- back/ukiki/src/main/java/project/domain/chat/dto/request/ChatDto.java
+++ back/ukiki/src/main/java/project/domain/chat/dto/request/ChatDto.java
@@ -3,10 +3,12 @@ package project.domain.chat.dto.request;
 import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import project.domain.chat.entity.ChatType;
 
 @Getter
 @Setter
+@ToString
 public class ChatDto {
 
     private String content;
